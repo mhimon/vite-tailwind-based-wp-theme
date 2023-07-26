27785f7854370586d6eb7e1b6c4ae04b2ab64e72
mhimon <mahbubhasanimon@gmail.com>
2023-07-26 12:57:56 +0600

    Fixes CONST

--- inc/vite.php
+++ inc/vite.php
@@ -10,31 +10,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'vtbt_DIST_URL', get_template_directory_uri() . '/dist' );
-define( 'vtbt_DIST_DIR', get_template_directory() . '/dist' );
-define( 'vtbt_VITE_DEV_SERVER', 'http://localhost:8098' );
-define( 'vtbt_VITE_ENTRY_POINT', 'app.js' );
-define( 'vtbt_VITE_DEV', true );
+define( 'VTBT_DIST_URL', get_template_directory_uri() . '/dist' );
+define( 'VTBT_DIST_DIR', get_template_directory() . '/dist' );
+define( 'VTBT_VITE_DEV_SERVER', 'http://localhost:8098' );
+define( 'VTBT_VITE_ENTRY_POINT', 'app.js' );
+define( 'VTBT_VITE_DEV', true );
 
 /**
  * Enqueue scripts and styles.
  */
 function vtbt_enqueue_vite_scripts() {
-	if ( defined( 'vtbt_VITE_DEV' ) && vtbt_VITE_DEV ) {
+	if ( defined( 'VTBT_VITE_DEV' ) && VTBT_VITE_DEV ) {
 		add_action( 'wp_head', function() {
-			echo '<script type="module" crossorigin src="' . vtbt_VITE_DEV_SERVER . '/' . vtbt_VITE_ENTRY_POINT . '"></script>';
+			echo '<script type="module" crossorigin src="' . VTBT_VITE_DEV_SERVER . '/' . VTBT_VITE_ENTRY_POINT . '"></script>';
 		});
 	} else {
 
-		$app_file_css = vtbt_DIST_DIR . '/css/app.css';
-		$app_file_js  = vtbt_DIST_DIR . '/js/main.js';
+		$app_file_css = VTBT_DIST_DIR . '/css/app.css';
+		$app_file_js  = VTBT_DIST_DIR . '/js/main.js';
 
 		if ( file_exists( $app_file_css ) ) {
-			wp_enqueue_style( 'vtbt-app', vtbt_DIST_URL . '/css/app.css', array(), filemtime( $app_file_css ) );
+			wp_enqueue_style( 'vtbt-app', VTBT_DIST_URL . '/css/app.css', array(), filemtime( $app_file_css ) );
 		}
 
 		if ( file_exists( $app_file_js ) ) {
-			wp_enqueue_script( 'vtbt-app', vtbt_DIST_URL . '/js/main.js', array( 'jquery' ), filemtime( $app_file_js ), true );
+			wp_enqueue_script( 'vtbt-app', VTBT_DIST_URL . '/js/main.js', array( 'jquery' ), filemtime( $app_file_js ), true );
 		}
 	}
 }

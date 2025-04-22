e605dad95c8168fd25479adec7968d7c87011215
Mahbub Hasan Imon <mahbubhasanimon@gmail.com>
2025-04-22 21:47:09 +0600

    Fixes

--- header.php
+++ header.php
@@ -3,9 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<?php wp_title(); ?>
-
 	
 	<?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
